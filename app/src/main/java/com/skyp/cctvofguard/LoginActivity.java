c1069811571863971eeac495781265a1834c826a
jun991203 <jun991203@naver.com>
2020-11-05 20:19:59 +0900

    로그인 화면 완료

--- app/src/main/java/com/skyp/cctvofguard/LoginActivity.java
+++ app/src/main/java/com/skyp/cctvofguard/LoginActivity.java
@@ -1,14 +1,41 @@
 package com.skyp.cctvofguard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    Button signInButton, signUpButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signInButton = findViewById(R.id.login_sign_in_Button);
+        signUpButton = findViewById(R.id.login_sign_up_Button);
+        ButtonAction();
+    }
+
+    public void ButtonAction(){
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ManagerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
