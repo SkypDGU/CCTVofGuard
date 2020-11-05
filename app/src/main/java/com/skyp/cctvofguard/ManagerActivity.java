05de0249d752de765fba8bbb26726e4ab6ceee9e
jun991203 <jun991203@naver.com>
2020-11-05 20:32:07 +0900

    Toolbar 연결 완료

--- app/src/main/java/com/skyp/cctvofguard/ManagerActivity.java
+++ app/src/main/java/com/skyp/cctvofguard/ManagerActivity.java
@@ -3,12 +3,26 @@ package com.skyp.cctvofguard;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class ManagerActivity extends AppCompatActivity {
+
+    /*Tool bar 관련 컴포넌트*/
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+
+        /*Tool bar 연결*/
+        toolbar = findViewById(R.id.manager_Toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
     }
 }
